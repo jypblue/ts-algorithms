2e641b8db93432730408908e27fb381473e80e52
wangzhaoxiong <wangzhaoxiong@yit.com>
2019-08-29 18:35:03 +0800

    add linkedList

---
+++ src/data-structures/linked-list.ts
@@ -0,0 +1,22 @@
+/**
+ * 链表
+ */
+
+import { defaultEquals, IEqualsFunction } from '../util';
+import { Node } from './models/linked-list-models';
+
+export default class LinkedList<T> {
+  protected count = 0;
+  protected head: Node<T> | undefined;
+
+  constructor(protected equalsFn: IEqualsFunction<T> = defaultEquals) { }
+
+  push(element:T) {
+    const node = new Node(element);
+    let current;
+
+    if (this.head === null) {
+
+    }
+  }
+}
