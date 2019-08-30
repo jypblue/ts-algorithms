0e8db971fd80d8de52693b65a07d7b4e3c417c0e
wangzhaoxiong <wangzhaoxiong@yit.com>
2019-08-30 17:27:38 +0800

    add linked-list

--- src/data-structures/linked-list.ts
+++ src/data-structures/linked-list.ts
@@ -1,5 +1,6 @@
 /**
  * 链表-基本链表
+ * 链表就像火车车厢的连接方式
  */
 
 import { defaultEquals, IEqualsFunction } from '../util';
@@ -11,12 +12,118 @@ export default class LinkedList<T> {
 
   constructor(protected equalsFn: IEqualsFunction<T> = defaultEquals) { }
 
-  push(element:T) {
+  // 向链表中添加元素
+  push(element: T) {
     const node = new Node(element);
     let current;
 
     if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
 
+      current.next = node;
     }
+    this.count++;
   }
+
+  // 获取链表节点，传入序号
+  getElementAt(index: number) {
+    if (index >= 0 && index <= this.count) {
+      let node = this.head;
+      for (let i = 0; i < index && node != null; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  insert(element:T, index:number) {
+    if  (index >= 0 && index <= this.count) {
+      const node = new Node(element);
+      if (index === 0) {
+        const current = this.head;
+        node.next = current;
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        node.next = previous.next;
+        previous.next = node;
+      }
+      this.count++;
+      return true;
+    }
+    return false;
+  }
+
+  removeAt(index: number) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  indexOf(element: T) {
+    let current = this.head;
+
+    for (let i = 0; i < this.count && current != null; i++) {
+      if (this.equalsFn(element, current.element)) {
+        return i;
+      }
+      current = current.next;
+    }
+    return -1;
+  }
+
+  remove(element:T) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  clear() {
+    this.head = undefined;
+    this.count = 0;
+  }
+
+  toString() {
+    if (this.head == null) {
+      return '';
+    }
+
+    let objString = `${this.head.element}`;
+    let current = this.head.next;
+
+    for (let i = 0; i < this.size() && current != null; i++) {
+      objString = `${objString},${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
+
 }
