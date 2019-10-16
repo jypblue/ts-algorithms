93afb6339f672f9394c90c41f9dabe4d09540be1
wangzhaoxiong <wangzhaoxiong@yit.com>
2019-10-16 11:24:04 +0800

    add hash table

--- src/data-structures/hash-table-linear-probing.ts
+++ src/data-structures/hash-table-linear-probing.ts
@@ -30,20 +30,82 @@ export default class HashTableLinearProbing<K, V> {
     return this.loseloseHashCode(key);
   }
 
+  // 存入元素
   put(key: K, value: V) {
+    if (key != null && value != null) {
+      const position = this.hashCode(key);
 
+      if (this.table[position] == null) {
+        this.table[position] = new ValuePair(key, value);
+      } else {
+        let index = position + 1;
+        while (this.table[index] != null) {
+          index++;
+        }
+        this.table[index] = new ValuePair(key, value);
+      }
+      return true;
+    }
+    return false;
   }
 
+  // 获取元素value
   get(key: K) {
-
+    const position = this.hashCode(key);
+    if (this.table[position] != null) {
+      if (this.table[position].key === key) {
+        return this.table[position].value;
+      }
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key) {
+        index++;
+      }
+      if (this.table[index] != null && this.table[index].key === key) {
+        return this.table[index].value;
+      }
+    }
+    return undefined;
   }
 
+  // 移除元素
   remove(key: K) {
+    const position = this.hashCode(key);
 
+    if (this.table[position] != null) {
+      if (this.table[position].key === key) {
+        delete this.table[position];
+        this.verifyRemoveSideEffect(key, position);
+        return true;
+      }
+
+      let index = position + 1;
+      while (this.table[index] != null && this.table[index].key !== key) {
+        index++;
+      }
+
+      if (this.table[index] != null && this.table[index].key === key) {
+        delete this.table[index];
+        this.verifyRemoveSideEffect(key, index);
+        return true;
+      }
+
+    }
+    return false;
   }
 
-  private verifyRemoveSideEffict(key: K, removedPosition: number) {
-
+  // 移动位置
+  private verifyRemoveSideEffect(key: K, removedPosition: number) {
+    const hash = this.hashCode(key);
+    let index = removedPosition + 1;
+    while (this.table[index] != null) {
+      const posHash = this.hashCode(this.table[index].key);
+      if (posHash <= hash || posHash <= removedPosition) {
+        this.table[removedPosition] = this.table[index];
+        delete this.table[index];
+        removedPosition = index;
+      }
+      index++;
+    }
   }
 
   isEmpty() {
