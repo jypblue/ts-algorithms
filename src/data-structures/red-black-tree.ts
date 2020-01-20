39638c2bc9805cbed02a8b7553bbbe194f3d5fde
wangzhaoxiong <wangzhaoxiong@yit.com>
2020-01-20 17:17:43 +0800

    add red black tree

--- src/data-structures/red-black-tree.ts
+++ src/data-structures/red-black-tree.ts
@@ -2,7 +2,7 @@ import {
   defaultCompare,
   ICompareFunction,
   Compare
-} '../util';
+} from '../util';
 import BinarySearchTree from './binary-search-tree';
 import { RedBlackNode, Colors } from './models/red-black-node';
 
@@ -103,16 +103,87 @@ export default class RedBlackTree<T> extends BinarySearchTree<T> {
 
   protected insertNode(node: RedBlackNode<T>, key: T): RedBlackNode<T> {
     if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
-
+      if (node.left == null) {
+        node.left = new RedBlackNode(key);
+        node.left.parent = node;
+        return node.left;
+      } else {
+        // 否则左查找
+        return this.insertNode(node.left, key);
+      }
     } else if (node.right == null) {
-
+      node.right = new RedBlackNode(key);
+      node.right.parent = node;
+      return node.right;
     } else {
-
+      // 右查找
+      return this.insertNode(node.right, key);
     }
   }
 
   private fixTreeProperties(node: RedBlackNode<T>) {
+    while (node && node.parent && node.parent.color === Colors.RED && node.color !== Colors.BLACK) {
+      let parent = node.parent;
+      const grandParent = parent.parent;
 
+      // case A
+      if (grandParent && grandParent.left === parent) {
+        const uncle = grandParent.right;
+
+        // case 1: 叔叔节点是红色 - 只重新赋值变色
+        if (uncle && uncle.isRed()) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else {
+          // case 2: node is right child - left rotate
+          if (node === parent.right) {
+            this.rotationRR(parent);
+            node = parent;
+            parent = node.parent;
+          }
+
+          // case 3: node is left child - right rotate
+          this.rotationLL(grandParent);
+          // swap color
+          parent.color = Colors.BLACK;
+          grandParent.color = Colors.RED;
+          node = parent;
+        }
+
+      } else {
+        // case b: parent is right child of grand parent
+        const uncle = grandParent.left; // 那么uncle就是在left
+
+        // case 1: uncle is read - only recoloring
+        if (uncle && uncle.isRed()) {
+          grandParent.color = Colors.RED;
+          parent.color = Colors.BLACK;
+          uncle.color = Colors.BLACK;
+          node = grandParent;
+        } else {
+          // case 2: node is left child - left rotate
+          if (node === parent.left) {
+            this.rotationLL(parent);
+            node = parent;
+            parent = node.parent;
+          }
+
+          // case 3: node is right child - left rotate
+          this.rotationRR(grandParent);
+          // swap color
+          parent.color = Colors.BLACK;
+          grandParent.color = Colors.RED;
+          node = parent;
+        }
+      }
+    }
+    this.root.color = Colors.BLACK;
+  }
+
+  getRoot() {
+    return this.root;
   }
 
 
